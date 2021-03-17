8ad7660f3719f86ec742a424f37bac4426456de1
dtracydepp <tracydepp82@gmail.com>
2021-03-17 15:29:02 -0500

    added Item details to render more info when link clicked..info is rendering

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,31 +1,40 @@
 import React from "react"
 import { Route } from "react-router-dom"
-import {ItemProvider} from "./items/ItemProvider.js"
-import {ItemList} from "./items/ItemList.js"
-import {SortFriends} from "../components/sort/SortButton.js"
-import {FriendProvider} from "./friends/FriendProvider.js"
-import {FriendList} from "./friends/FriendList.js"
+import { ItemProvider } from "./items/ItemProvider.js"
+import { ItemList } from "./items/ItemList.js"
+import { SortFriends } from "../components/sort/SortButton.js"
+import { FriendProvider } from "./friends/FriendProvider.js"
+import { FriendList } from "./friends/FriendList.js"
+import { ItemDetail } from "./items/ItemDetail.js"
 
 export const ApplicationViews = () => {
     return (
         <>
             {/* Render the item list when http://localhost:3000/ */}
             <ItemProvider>
+                <FriendProvider>
                 <Route exact path="/">
                     <ItemList />
                 </Route>
+
+                <Route exact path="/items/detail/:itemId(\d+)">
+                    <ItemDetail />
+                </Route>
+                </FriendProvider>
             </ItemProvider>
-            
+
             <Route exact path="/" render={props => <SortFriends {...props} />} />
 
             <Route exact path="/items">
-                   
+
             </Route>
 
             <FriendProvider>
                 <Route exact path="/allfriends">
                     <FriendList />
                 </Route>
+
+
             </FriendProvider>
         </>
     )
