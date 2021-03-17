b22b53e8f07f51bd4783a6758a51c682e7a48e50
dtracydepp <tracydepp82@gmail.com>
2021-03-17 12:01:23 -0500

    added Sort by button and routing...rendering button to DOM

--- src/components/items/ItemCard.js
+++ src/components/items/ItemCard.js
@@ -1,10 +1,14 @@
 import React from "react"
 import "./Item.css"
-// import {Link} from "react-router-dom"
+import {Link} from "react-router-dom"
 
 export const ItemCard = ({item}) => (
-    <section className="item">
-        <h3 className="item__name">{item.itemName}</h3>
-        <div className="item__description">{item.description}</div>
+    <section className="items">
+          <h3 className="item__name">
+        <Link to={`/items/detail/${item.id}`}>
+          { item.itemName }
+        </Link>
+    </h3>
+    <div className="item__description">{item.description}</div>
     </section>
 ) 
---
+++ src/components/sort/SortButton.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "../sort/SortButton.css"
+
+export const SortFriends = (props) => {
+    return (
+        <>
+            
+            
+            <button onClick={() => props.history.push("/allfriends")}>
+                Sort by Friends
+            </button>
+        </>
+    )
+}
