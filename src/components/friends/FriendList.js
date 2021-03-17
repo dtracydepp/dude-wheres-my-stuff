7a6f3e5f114d526c0fffba390c107d9e95cab2b2
dtracydepp <tracydepp82@gmail.com>
2021-03-17 16:31:46 -0500

    select element is rendering

--- src/components/friends/FriendList.js
+++ src/components/friends/FriendList.js
@@ -1,6 +1,6 @@
-import React, {useContext, useEffect} from "react"
-import {FriendsContext} from "./FriendProvider.js"
-import {FriendCard} from "./FriendCard.js"
+import React, { useContext, useEffect } from "react"
+import { FriendsContext } from "./FriendProvider.js"
+import { FriendCard } from "./FriendCard.js"
 import "./Friend.css"
 import { useHistory } from "react-router-dom"
 
@@ -9,30 +9,28 @@ export const FriendList = () => {
   //  // The useHistory hook tells React which route to visit. Tells React to render the friend form component (not added yet)
   const history = useHistory()
 
-    // This state changes when `getFriends()` is invoked below
-    const { friends, getFriends } = useContext(FriendsContext)
-  
-    //useEffect - reach out to the world for something - API call for the friends; will only run one time at intial render because array is empty
-    useEffect(() => {
-      console.log("FriendList: useEffect - getFriends")
-      getFriends()
+  // This state changes when `getFriends()` is invoked below
+  const { friends, getFriends } = useContext(FriendsContext)
 
-    }, [])
-  
-  
-    return (
-        <div className="friends">
-            <h3>My Friends</h3>
-          {console.log("FriendsList: Render", friends)}
+  //useEffect - reach out to the world for something - API call for the friends; will only run one time at intial render because array is empty
+  useEffect(() => {
+    console.log("FriendList: useEffect - getFriends")
+    getFriends()
 
-          {/* Return drop down with all friends and items they borrowed */}
-          
-            {
-            friends.map((friend) => {
-              //   key and friend become properties on the object passed in as in argument
-              return <FriendCard key={friend.id} friend={friend} />
-            })
-        }
-        </div>
-      )
-    } 
+  }, [])
+
+
+  return (
+    <div className="friends">
+      <h3>My Friends</h3>
+      {/* {console.log("FriendsList: Render", friends)} */}
+
+  
+    <select>
+      <option value="0">Select a friend</option>
+    {/* Not sure on getting date, need to add getFriendbyId */}
+      </select>
+    </div>
+  )
+
+  }
