db79ced727c42f1f9a7a0513aa7f3d1af6206c4c
dtracydepp <tracydepp82@gmail.com>
2021-03-17 16:48:13 -0500

    added comment

--- src/components/items/ItemDetail.js
+++ src/components/items/ItemDetail.js
@@ -24,7 +24,7 @@ export const ItemDetail = () => {
       <h3 className="item__name">{items.itemName}</h3>
       {console.log(items.itemName)}
       <div className="item__description">{items.description}</div>
-      {/* NEED friendName to render not ID, need a getFriendbyID?, researching friends is  an array so use .map to get name */}
+      {/* NEED friendName to render not ID, need a getFriendbyID? .find or .map with friendid=id?, researching friends is  an array so use .map to get name */}
       <div className="friend__name">Who Has My Stuff: {items.friendId}</div> 
       <button onClick={() => {history.push("/items/create")}}>
             Add Note
